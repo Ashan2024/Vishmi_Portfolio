531343c4445fa7d1a1f01b5ea5e0c912e108f7a2
Ashan karunathilaka <166946482+Ashan2024@users.noreply.github.com>
2025-12-26 21:02:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Scroll to projects section
+document.getElementById("projectBtn").addEventListener("click", () => {
+  document.getElementById("projects").scrollIntoView({
+    behavior: "smooth"
+  });
+});
